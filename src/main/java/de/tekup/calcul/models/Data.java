163b7fd141e1574c8ca6b93bb967146e84d0f8a9
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-09-21 15:53:54 +0100

    TODO

---
+++ src/main/java/de/tekup/calcul/models/Data.java
@@ -0,0 +1,25 @@
+package de.tekup.calcul.models;
+
+import java.util.List;
+
+public class Data {
+	
+	private List<Integer> op1;
+	private List<Integer> op2;
+
+	public List<Integer> getOp1() {
+		return op1;
+	}
+	public void setOp1(List<Integer> op1) {
+		this.op1 = op1;
+	}
+	public List<Integer> getOp2() {
+		return op2;
+	}
+	public void setOp2(List<Integer> op2) {
+		this.op2 = op2;
+	}
+	
+	
+
+}
