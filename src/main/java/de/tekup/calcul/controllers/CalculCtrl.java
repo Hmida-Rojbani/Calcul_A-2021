d53279e7a244d0eb9da98945d4f4629695743637
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-09-21 16:17:16 +0100

    sum by list

--- src/main/java/de/tekup/calcul/controllers/CalculCtrl.java
+++ src/main/java/de/tekup/calcul/controllers/CalculCtrl.java
@@ -1,9 +1,14 @@
 package de.tekup.calcul.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.tekup.calcul.models.Data;
 import de.tekup.calcul.services.CalculService;
 
 @RestController
@@ -19,6 +24,11 @@ public class CalculCtrl {
 	@RequestMapping(path = "/add/{v1}/{v2}")
 	public int addCalc(@PathVariable("v1") int a, @PathVariable("v2") int b) {
 		return service.sum(a, b);
+	}
+	
+	@PostMapping("/add")
+	public List<Integer> addBatchCalc(@RequestBody Data data){
+		return service.sumBatch(data);
 	}
 
 }
--- src/main/java/de/tekup/calcul/services/CalculService.java
+++ src/main/java/de/tekup/calcul/services/CalculService.java
@@ -1,12 +1,26 @@
 package de.tekup.calcul.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import de.tekup.calcul.models.Data;
 
 @Service
 public class CalculService {
 	
 	public int sum(int a, int b) {
 		return a+b;
+	}
+	
+	public List<Integer> sumBatch(Data data){
+		List<Integer> res = new ArrayList<>();
+		for (int i = 0; i < data.getOp1().size(); i++) {
+			res.add(data.getOp1().get(i) + data.getOp2().get(i));
+		}
+		
+		return res;
 	}
 
 }
